074289b4d90dee653d56abc3235d35cdf56a8bfa
Nikholas Borges <nikholas.borges@gmail.com>
2024-06-30 18:55:52 -0300

    fix temp folder creation on build

--- src/web_app/app.py
+++ src/web_app/app.py
@@ -10,6 +10,7 @@ flask_app = Flask(__name__)
 # Configure upload folder
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 flask_app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 logger = get_logger(__name__)
